7b0245c429f0e205276e7f5ebbdd404f19e6490b
Rajneesh Tailor <rajneesh071@gmail.com>
2020-11-24 17:07:32 +0530

    Initial Commit

---
+++ AppleSearchAPIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppleSearchAPIDemo
+//
+//  Created by Rajneesh on 24/11/20.
+//  Copyright © 2020 BRRV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
