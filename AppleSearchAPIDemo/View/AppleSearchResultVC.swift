d01a2ae84925df8346dde739a9afd880dcc357b5
Rajneesh Tailor <rajneesh071@gmail.com>
2020-12-11 12:58:11 +0530

    Some changes for release configuration

--- AppleSearchAPIDemo/View/AppleSearchResultVC.swift
+++ AppleSearchAPIDemo/View/AppleSearchResultVC.swift
@@ -107,7 +107,6 @@ extension AppleSearchResultVC: UISearchBarDelegate {
     }
 }
 
-#if DEBUG
 extension AppleSearchResultVC {
     func exposePrivateTableView() -> UITableView {
         return self.tableView
@@ -116,4 +115,3 @@ extension AppleSearchResultVC {
         return self.searchController
     }
 }
-#endif
